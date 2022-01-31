5d2738179ea49aae916e2b8d458203b758e3c45f
Ajay <Ajy9582@gmail.com>
2022-01-31 17:20:13 +0530

    cc

--- src/Welcome/welcome.js
+++ src/Welcome/welcome.js
@@ -11,10 +11,27 @@ const welcome = () => {
       <Box sx={{ display: "inline-grid", width: "100%" }}>
         <Box className="Body" sx={{}}>
           <Grid container spacing={2}>
-            <Grid item xs={2} sx={{ p: 2 }}>
-              Hi
+            <Grid
+              item
+              xs={4}
+              style={{
+                paddingTop: "10%",
+                paddingLeft: "3%",
+                paddingRight: "5%",
+              }}
+            >
+              <h1>
+                Prepare,
+                <br /> and get the Job you want..
+              </h1>
+              <h3>Craft a killer Tech Resume and land your dream job.</h3>
             </Grid>
-            <Grid item xs={10} sx={{ p: 2 }}></Grid>
+            <Grid item xs={8} sx={{ p: 2 }}>
+              <img
+                src={Image}
+                style={{ objectFit: "contain", width: "100%" }}
+              />
+            </Grid>
           </Grid>
         </Box>
       </Box>
